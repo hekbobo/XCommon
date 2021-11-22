a18d1bad896c0b524a99c7dcecaaa07d56c2d797
zyb <zyb@shoot.net.cn>
2021-11-22 16:24:45 +0800

    + 增加地boom

--- app/src/main/java/com/b/xcommon/MainActivity.java
+++ app/src/main/java/com/b/xcommon/MainActivity.java
@@ -1,20 +1,34 @@
 package com.b.xcommon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
-import com.shoot.XBase64Util;
+import com.nightonke.boommenu.ButtonEnum;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MAIN_ACTIVITY = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonEnum x;
 
-        String rest =  XBase64Util.decodeToString("eyJ0eXBlIjo2LCJleHRyYSI6IiIsImRsZyI6eyJ0aXRsZSI6IuaPkOekuiIsIm1lc3NhZ2UiOiLlvZPliY3mmK/mvJTnpLrotKblj7fvvIzor7flhYjms6jlhowiLCJsZWZ0QnRuIjoi55m75b2VIiwicmlnaHRCdG4iOiLnq4vljbPms6jlhowiLCJhY3Rpb24iOjF9fQ==");
-        Log.e("a", rest);
+        KLogWrap.init(KLockerLogHelper.getInstance(this));
+        KLogWrap.error("Tag", "XXXX");
 
+
+//        String rest =  XBase64Util.decodeToString("eyJ0eXBlIjo2LCJleHRyYSI6IiIsImRsZyI6eyJ0aXRsZSI6IuaPkOekuiIsIm1lc3NhZ2UiOiLlvZPliY3mmK/mvJTnpLrotKblj7fvvIzor7flhYjms6jlhowiLCJsZWZ0QnRuIjoi55m75b2VIiwicmlnaHRCdG4iOiLnq4vljbPms6jlhowiLCJhY3Rpb24iOjF9fQ==");
+//        Log.e(MAIN_ACTIVITY, rest);
+//
+//        Log.e(MAIN_ACTIVITY,Environment.getExternalStorageDirectory().getAbsolutePath());
+//        Log.e(MAIN_ACTIVITY,Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath());
+//
+//        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+//        XFileUtils.getAllFiles(path+ "/android/data/com.tencent.mm/", "pdf");
+//        XFileUtils.getAllFiles(path+ "/android/data/com.tencent.mm/MicroMsg/Download/", "pdf");
     }
 }
